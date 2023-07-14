9ce502357165e8728a6b5ecab01e7e1e40274484
Alan Miller <183872+alantmiller@users.noreply.github.com>
2023-07-14 02:32:56 -0700

    Update Sitemap.php

--- src/PhpSitemapGenerator/Sitemap.php
+++ src/PhpSitemapGenerator/Sitemap.php
@@ -1,34 +1,49 @@
 <?php
-// src/PhpSitemapGenerator/Sitemap.php 
-
 namespace PhpSitemapGenerator;
 
-class Sitemap
+class Sitemap 
 {
 
-    protected $conf;
-    protected $entries = array();
+  // Properties
+  protected Config $config;
+
+  protected array $entries = [];
+
+  // Constructor
+  public function __construct()
+  {
+    $this->config = new Config();
+  }
+
+  // Methods
+  public function addEntry(string $loc, string $priority, string $changefreq = '', string $lastmod = ''): void
+  {
+    $entry = new Entry($loc, $priority, $changefreq, $lastmod);
+    $this->entries[] = $entry;
+  }
+
+  public function setDomain(string $domain): void
+  {
+    $this->config->setDomain($domain);
+  }
+
+  public function toString(): string
+  {
+    $this->config->setEntries($this->entries);
     
-    public function __construct()
-    {
-        $this->conf = new Config();   
-    }
-    
-    public function addEntry($loc, $priority, $changefreq="", $lastmod='')
-    {
-        $this->entries[] = new Entry($loc, $priority, $changefreq, $lastmod);   
-    }
-    
-    public function setDomain($domain)
-    {
-        $this->conf->setDomain($domain); 
-    }
-    
-    public function toString()
-    {
-        $this->conf->setEntries($this->entries);
-        $generator = new Generator($this->conf);
-        return $generator->toString();
-    }
+    $generator = new Generator($this->config);
+    return $generator->toString();
+  }
+
+  // Getters
+  public function getConfig(): Config
+  {
+    return $this->config;
+  }
+
+  public function getEntries(): array
+  {
+    return $this->entries;
+  }
 
 }
