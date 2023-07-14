6401883a0d64a3328d29971748e2f2fe82f17f66
Alan Miller <183872+alantmiller@users.noreply.github.com>
2023-07-14 01:39:24 -0700

    Update Entry.php

--- src/PhpSitemapGenerator/Entry.php
+++ src/PhpSitemapGenerator/Entry.php
@@ -1,5 +1,5 @@
 <?php 
-// src/PhpSitemapGenerator/Entry.php
+// src/PhpSitemapGenerator/Entry.php  
 
 namespace PhpSitemapGenerator;
 
@@ -28,8 +28,23 @@ class Entry
 
     public function get($arg)
     {
-        // Additional code removed for brevity
-        
+        switch($arg)
+        {
+            case 'loc':
+                return $this->_getLoc();
+            case 'priority':
+                return $this->_getPriority();
+            case 'changefreq':
+                return $this->_getChangefreq();
+            case 'lastmod':
+                return $this->_getLastmod();
+            case 'frequencies':
+                return $this->_frequencies;
+            case 'priorities':
+                return $this->_priorities;
+            default:
+                throw new Exception('get() method in class: '.__CLASS__.' did not recognize the argument');
+        }
     }
 
     private function _setLoc($loc)
@@ -40,22 +55,72 @@ class Entry
 
     private function _setPriority($priority)
     {
-         // Additional code removed for brevity
-         
+        $priority = trim($priority);
+        
+        if (!in_array($priority, $this->_priorities)) {
+            throw new Exception('setPriority() method is expecting a value between 0.0 and 1.0');
+        } else {
+            $this->_priority = $priority;
+        }
+        return $this;
     }
 
     private function _setChangefreq($changefreq)
     {
-       // Additional code removed for brevity
-       
+        $changefreq = strtolower(trim($changefreq));
+
+        if (!in_array($changefreq, $this->_frequencies)) {
+            throw new Exception('setChangefreq() method is expecting a value such as hourly daily, weekly etc');
+        } else {
+            $this->_changefreq = $changefreq;
+        }
+        return $this;
     }
 
     private function _setLastmod($lastmod)
     {
-       // Additional code removed for brevity
-       
+        $arr = date_parse($lastmod);
+        if (!checkdate($arr['month'], $arr['day'], $arr['year'])) {
+            throw new Exception('setLastmod() method expects a valid date');
+        } else {
+            $arr['month'] = str_pad($arr['month'], 2, "0", STR_PAD_LEFT);
+            $arr['day'] = str_pad($arr['day'], 2, "0", STR_PAD_LEFT);
+            $this->_lastmod =
+                sprintf('%s-%s-%s',$arr['year'], $arr['month'], $arr['day']);
+        }
+        return $this;
     }
 
-   // Additional methods removed for brevity
+    private function _getLoc()
+    {
+        return $this->_loc;
+    }
 
+    private function _getPriority()
+    {
+        if (!strlen($this->_priority) > 0) {
+            return  '0.5';
+        } else {
+            return $this->_priority;
+        }
+    }
+
+    private function _getLastmod()
+    {
+        // set default values
+        if (!strlen($this->_lastmod) > 0) {
+             return date('Y-m-d');
+        } else {
+            return $this->_lastmod;
+        }
+    }
+
+    private function _getChangefreq()
+    {
+        if (!strlen($this->_changefreq) > 0) {
+            return 'monthly';
+        } else {
+            return $this->_changefreq;
+        }
+    }
 }
